8628af1596fb02dc622af8b9824d836c0b635bb2
cyc <1037179635@qq.com>
2023-06-13 18:11:27 +0800

    完成词法分析器输出

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -4,16 +4,34 @@ import symbols.Type;
 
 import java.io.IOException;
 import java.util.Hashtable;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 public class Lexer {
     public static int line = 1;
     public String Code;
     public int now;
     char peek = ' ';
     Hashtable words = new Hashtable();
+    Hashtable keywords = new Hashtable();
+    private static String filePath="src/out/LexerOut.txt";
 
     void reserve(Word w) {
         words.put(w.lexeme, w);
+    }
+    public void lexerPrint(String output) {
+        try (FileWriter fileWriter = new FileWriter(filePath,true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(output);
+           // System.out.println("内容已成功写入文件：" + filePath);
+        } catch (IOException e) {
+            System.out.println("写入文件时发生错误：" + e.getMessage());
+        }
+        System.out.print(output);
     }
     //初始化单词的tag
     public Lexer(String code) {
@@ -29,6 +47,7 @@ public class Lexer {
         reserve(Type.Char);
         reserve(Type.Bool);
         reserve(Type.Float);
+        keywords=words;
         now=0;
         Code=code;
     }
@@ -53,23 +72,71 @@ public class Lexer {
         }
         switch (peek) {
             case '&':
-                if (readch('&')) return Word.and;
-                else return new Token('&');
+                if (readch('&')) 
+                {
+                    lexerPrint("operator: &&\n");
+                    return Word.and;
+                }
+                else 
+                {
+                    lexerPrint("operator: &\n");
+                    return new Token('&');
+                }
             case '|':
-                if (readch('|')) return Word.or;
-                else return new Token('|');
+                if (readch('|')) 
+                {
+                    lexerPrint("operator: ||\n");
+                    return Word.or;
+                }
+                else 
+                {
+                    lexerPrint("operator: |\n");
+                    return new Token('|');
+                }
             case '=':
-                if (readch('=')) return Word.eq;
-                else return new Token('=');
+                if (readch('=')) 
+                {
+                    lexerPrint("operator: ==\n");
+                    return Word.eq;
+                }
+                else 
+                {
+                    lexerPrint("operator: =\n");
+                    return new Token('=');
+                }
             case '!':
-                if (readch('=')) return Word.ne;
-                else return new Token('!');
+                if (readch('=')) 
+                {
+                    lexerPrint("operator: !=\n");
+                    return Word.ne;
+                }
+                else 
+                {
+                    lexerPrint("operator: !\n");
+                    return new Token('!');
+                }
             case '<':
-                if (readch('=')) return Word.le;
-                else return new Token('<');
+                if (readch('=')) 
+                {
+                    lexerPrint("operator: <=\n");
+                    return Word.le;
+                }
+                else 
+                {
+                    lexerPrint("operator: <\n");
+                    return new Token('<');
+                }
             case '>':
-                if (readch('=')) return Word.ge;
-                else return new Token('>');
+                if (readch('=')) 
+                {
+                    lexerPrint("operator: >=\n");
+                    return Word.ge;
+                }
+                else 
+                {
+                    lexerPrint("operator: >\n");
+                    return new Token('>');
+                }
         }
         if (Character.isDigit(peek) || peek == '.') {
             int v = 0;
@@ -78,7 +145,11 @@ public class Lexer {
                     v = 10 * v + Character.digit(peek, 10);
                     readch();
                 } while (Character.isDigit(peek));
-            if (peek != '.') return new Num(v);
+            if (peek != '.')
+            {
+                lexerPrint("const value: "+Integer.toString(v)+"\n");
+                return new Num(v);
+            } 
             float x = v, d = 10;
             for (; ; ) {
                 readch();
@@ -86,6 +157,7 @@ public class Lexer {
                 x = x + Character.digit(peek, 10) / d;
                 d = d * 10;
             }
+            lexerPrint("constant: "+Float.toString(x)+"\n");
             return new Real(x);
         }
         if (Character.isLetter(peek) || peek == '_') {
@@ -96,12 +168,23 @@ public class Lexer {
             } while (Character.isLetterOrDigit(peek) || peek == '_');
             String s = b.toString();
             Word w = (Word) words.get(s);
-            if (w != null) return w;
+            if (w != null)
+            {
+                Word keyword=(Word) keywords.get(s);
+                if(keyword!=null)lexerPrint("key word: "+s+"\n");
+                else lexerPrint("identifier: "+s+"\n");
+                return w;
+            }
             w = new Word(s, Tag.ID);
             words.put(s, w);
+            lexerPrint("identifier: "+s+"\n");
             return w;
         }
         Token tok = new Token(peek);
+        if(peek=='$')
+            lexerPrint("EOF symbol: "+peek+"\n");
+        else
+            lexerPrint("delimiter: "+peek+"\n");
         peek = ' ';
         return tok;
     }
