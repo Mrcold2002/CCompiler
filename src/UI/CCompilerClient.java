7abc8b932d737aa13a980ff3c5689a72d1282e0c
Mr_cold <541892537@qq.com>
2023-06-13 17:51:03 +0800

    feat:客户端界面调整

--- src/UI/CCompilerClient.java
+++ src/UI/CCompilerClient.java
@@ -13,7 +13,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,14 +26,18 @@ import javax.swing.SwingUtilities;
 public class CCompilerClient {
     private JFrame frame;
     private JTextArea codeTextArea;
-    private JTextArea outputTextArea;
-    private JTextArea additionalOutputTextArea;
+    private JTextArea lexerOutputTextArea;
+    private JTextArea parserOutputTextArea;
+    private JTextArea midCodeOutputTextArea;
+    private String midCodeOutPath = "src/out/midCodeOut.txt";
+    private String lexerOutPath = "src/out/LexerOut.txt";
+    private String parserOutPath = "src/out/ParserOut.txt";
 
     public CCompilerClient() {
         // 创建 JFrame 实例
         frame = new JFrame("C语言编译器客户端");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(1000, 800);
 
         // 创建面板
         JPanel panel = new JPanel(new GridLayout(1, 2, 5, 5));
@@ -50,14 +53,41 @@ public class CCompilerClient {
         panel.add(inputPanel);
 
         // 创建右侧输出区域
-        JPanel outputPanel = new JPanel(new BorderLayout());
-        JLabel outputLabel = new JLabel("out", SwingConstants.CENTER);
-        outputLabel.setFont(new Font("Arial", Font.BOLD, 14));
-        outputPanel.add(outputLabel, BorderLayout.NORTH);
-        outputTextArea = new JTextArea();
-        outputTextArea.setEditable(false);
-        JScrollPane outputScrollPane = new JScrollPane(outputTextArea);
-        outputPanel.add(outputScrollPane, BorderLayout.CENTER);
+        JPanel outputPanel = new JPanel(new GridLayout(3, 1, 5, 5));
+
+        // 创建词法分析输出区域
+        JPanel lexerOutputPanel = new JPanel(new BorderLayout());
+        JLabel lexerOutputLabel = new JLabel("lexerOut", SwingConstants.CENTER);
+        lexerOutputLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        lexerOutputPanel.add(lexerOutputLabel, BorderLayout.NORTH);
+        lexerOutputTextArea = new JTextArea();
+        lexerOutputTextArea.setEditable(false);
+        JScrollPane lexerOutputScrollPane = new JScrollPane(lexerOutputTextArea);
+        lexerOutputPanel.add(lexerOutputScrollPane, BorderLayout.CENTER);
+        outputPanel.add(lexerOutputPanel);
+
+        // 创建语法分析输出区域
+        JPanel parserOutputPanel = new JPanel(new BorderLayout());
+        JLabel parserOutputLabel = new JLabel("parserOut", SwingConstants.CENTER);
+        parserOutputLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        parserOutputPanel.add(parserOutputLabel, BorderLayout.NORTH);
+        parserOutputTextArea = new JTextArea();
+        parserOutputTextArea.setEditable(false);
+        JScrollPane parserOutputScrollPane = new JScrollPane(parserOutputTextArea);
+        parserOutputPanel.add(parserOutputScrollPane, BorderLayout.CENTER);
+        outputPanel.add(parserOutputPanel);
+
+        // 创建中间代码输出区域
+        JPanel midCodeOutputPanel = new JPanel(new BorderLayout());
+        JLabel midCodeOutputLabel = new JLabel("midCodeOut", SwingConstants.CENTER);
+        midCodeOutputLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        midCodeOutputPanel.add(midCodeOutputLabel, BorderLayout.NORTH);
+        midCodeOutputTextArea = new JTextArea();
+        midCodeOutputTextArea.setEditable(false);
+        JScrollPane midCodeOutputScrollPane = new JScrollPane(midCodeOutputTextArea);
+        midCodeOutputPanel.add(midCodeOutputScrollPane, BorderLayout.CENTER);
+        outputPanel.add(midCodeOutputPanel);
+
         panel.add(outputPanel);
 
         // 将面板添加到窗口
@@ -81,34 +111,46 @@ public class CCompilerClient {
         frame.setVisible(true);
     }
 
-    private void compileAndExecute(String code) throws IOException {
-
-        String filePath = "src/out/midCodeOut.txt"; // 替换为你的文本文件路径
-        //清空输出
-        try (FileWriter fileWriter = new FileWriter(filePath, false);
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+    // 清空输出文件内容
+    private void clearOut(String filePath) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
             bufferedWriter.write("");
             System.out.println("文件内容已成功清空：" + filePath);
         } catch (IOException e) {
             System.out.println("清空文件内容时发生错误：" + e.getMessage());
         }
+    }
 
+    // 运行按钮绑定后的函数
+    private void compileAndExecute(String code) throws IOException {
+        // 清空输出文件内容
+        clearOut(midCodeOutPath);
+        clearOut(lexerOutPath);
+        clearOut(parserOutPath);
+
+        // 开始运行
         Lexer lex = new Lexer(code + "$");
         Parser parse = new Parser(lex);
         parse.program();
 
+        // 设置词法分析输出
+        String lexerFileContent = readFileContent(lexerOutPath);
+        lexerOutputTextArea.setText(lexerFileContent);
 
-        // 读取文本文件内容
+        // 设置语法分析输出
+        String parserFileContent = readFileContent(parserOutPath);
+        parserOutputTextArea.setText(parserFileContent);
 
-        String fileContent = readFileContent(filePath);
-
-        // 设置outputTextArea的文本为文件内容
-        outputTextArea.setText(fileContent);
+        // 设置中间代码输出
+        String midCodeFileContent = readFileContent(midCodeOutPath);
+        midCodeOutputTextArea.setText(midCodeFileContent);
     }
 
+    // 读取文件内容
     private String readFileContent(String filePath) throws IOException {
         StringBuilder contentBuilder = new StringBuilder();
-        Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8).forEach(line -> contentBuilder.append(line).append("\n"));
+        Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8)
+                .forEach(line -> contentBuilder.append(line).append("\n"));
         return contentBuilder.toString();
     }
 
